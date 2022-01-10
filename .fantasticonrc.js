b109681cefcbb03444fcce7383ae769cda94751d
camargo <Christopher.A.Camargo@jpl.nasa.gov>
2022-01-10 14:04:29 -0800

    refactor: reorganize to release open source
    - Add icons and example
    - Update dependenceis
    - Add license

---
+++ .fantasticonrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  assetTypes: ["css", "html"],
+  fontTypes: ["woff2", "woff"],
+  inputDir: "./icons",
+  name: "stellar-icons",
+  outputDir: "./icons",
+  prefix: "si",
+  selector: ".si",
+  templates: { css: "./icons/css.hbs" },
+};
