7970f1e7d77201a0a436d8f01ac507b12bb99722
Lyle Klyne <lyle.klyne@gmail.com>
2024-11-07 14:50:05 -0800

    wip landing page copy

--- packages/svelte/src/routes/(app)/+page.svelte
+++ packages/svelte/src/routes/(app)/+page.svelte
@@ -1,45 +1,89 @@
 <script lang="ts">
-  import {
-    Alert,
-    AlertDialog,
-    AspectRatio,
-    Badge,
-    Breadcrumb,
-    Button,
-    DropdownMenu,
-    H1,
-    H2,
-    H3,
-    H4,
-    ModeWatcher,
-    P,
-    Popover,
-    ThemeSwitcherButton,
-    ThemeSwitcherDropdown,
-  } from '$lib/index.js';
-  import { Terminal } from 'lucide-svelte';
+  import { Button, ModeWatcher, Tabs } from '$lib/index.js';
 </script>
 
 <ModeWatcher />
 
-<div class="flex flex-col gap-4 bg-background p-16">
-  <div class="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]">
-    Components for powerful apps
+<div class="flex flex-col items-center gap-4 bg-background p-16 container mx-auto">
+  <div class="flex flex-col gap-4 items-center">
+    <div class="text-6xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]">
+      Launch powerful space tools
+    </div>
+
+    <p class="text-xl text-muted-foreground font-light max-w-2xl text-center">
+      Flexible React and Svelte components to quickly design and build high-quality, accessible web applications. Built
+      with Shadcn/ui, Radix, and more.
+    </p>
+    <div class="flex gap-4 my-4">
+      <Button size="lg">Get started</Button><Button variant="secondary" size="lg">Components</Button>
+    </div>
   </div>
-  <p class="text-lg font-light">
-    Flexible React and Svelte components to create powerful, accessible, and easy-to-use web tools.
-  </p>
-  <div>
-    Button
+  <!-- <div class="flex flex-col gap-4">
+    <div
+      class="text-muted-foreground text-sm font-medium flex bg-secondary px-3 py-1 rounded-lg border shadow-inner border-secondary flex-col items-center gap-4"
+    >
+      Open source from NASA's Jet Propulsion Laboratory
+    </div>
+    <div class="text-2xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]">
+      User Interface Standards
+    </div>
+    <p class="text-xl text-muted-foreground font-light max-w-2xl text-center">
+      Flexible React and Svelte components to quickly design and build high-quality, accessible web applications. Built
+      with Shadcn/ui, Radix, and more.
+    </p>
+    <div class="flex gap-4 my-4">
+      <Button size="lg">Get started</Button><Button variant="secondary" size="lg">Components</Button>
+    </div>
+  </div> -->
+  <div class="w-full mt-8">
+    <Tabs.Root value="dashboard-1" class="w-full">
+      <Tabs.List class="rounded-t-mdgap-4 bg-background">
+        <Tabs.Trigger class="data-[state=active]:bg-secondary rounded-full px-4" value="dashboard-1"
+          >Dashboard 1</Tabs.Trigger
+        >
+        <Tabs.Trigger class="data-[state=active]:bg-secondary rounded-full px-4" value="dashboard-2"
+          >Dashboard 2</Tabs.Trigger
+        >
+        <Tabs.Trigger class="data-[state=active]:bg-secondary rounded-full px-4" value="dashboard-3"
+          >Dashboard 3</Tabs.Trigger
+        >
+        <Tabs.Trigger class="data-[state=active]:bg-secondary rounded-full px-4" value="inputs">Inputs</Tabs.Trigger>
+        <Tabs.Trigger class="data-[state=active]:bg-secondary rounded-full px-4" value="charts">Charts</Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="dashboard-1">
+        <div
+          class="w-full h-[800px] bg-secondary flex items-center justify-center text-muted-foreground font-mono uppercase"
+        >
+          Dashboard 1
+        </div>
+      </Tabs.Content>
+      <Tabs.Content value="dashboard-2">
+        <div
+          class="w-full h-[800px] bg-secondary flex items-center justify-center text-muted-foreground font-mono uppercase"
+        >
+          Dashboard 2
+        </div>
+      </Tabs.Content>
+      <Tabs.Content value="dashboard-3">
+        <div
+          class="w-full h-[800px] bg-secondary flex items-center justify-center text-muted-foreground font-mono uppercase"
+        >
+          Dashboard 3
+        </div>
+      </Tabs.Content>
+      <Tabs.Content value="inputs">Inputs</Tabs.Content>
+      <Tabs.Content value="charts">Charts</Tabs.Content>
+    </Tabs.Root>
+    <!-- Button
     <div class="flex items-center gap-4">
       <Button on:click={() => console.log(1)}>Click me</Button>
       <Button variant="secondary" on:click={() => console.log(1)}>Click me</Button>
       <Button variant="outline" on:click={() => console.log(1)}>Click me</Button>
       <Button size="sm" on:click={() => console.log(1)}>Click me</Button>
       <Button size="lg" on:click={() => console.log(1)}>Click me</Button>
-    </div>
+    </div> -->
   </div>
-  <div>
+  <!-- <div>
     Typography
     <H1>Heading 1</H1>
     <H2>Heading 2</H2>
@@ -158,5 +202,5 @@
         </Breadcrumb.List>
       </Breadcrumb.Root>
     </div>
-  </div>
+  </div> -->
 </div>
