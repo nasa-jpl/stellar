9c50109b0fde1b5368701b6064fe74bc4a540954
Lyle Klyne <lyle.klyne@gmail.com>
2024-11-08 13:19:05 -0800

    add table cell size variants

--- packages/svelte/src/lib/components/ui/table/index.ts
+++ packages/svelte/src/lib/components/ui/table/index.ts
@@ -1,28 +1,50 @@
-import Root from "./table.svelte";
-import Body from "./table-body.svelte";
-import Caption from "./table-caption.svelte";
-import Cell from "./table-cell.svelte";
-import Footer from "./table-footer.svelte";
-import Head from "./table-head.svelte";
-import Header from "./table-header.svelte";
-import Row from "./table-row.svelte";
+import { type VariantProps, tv } from 'tailwind-variants';
+import Body from './table-body.svelte';
+import Caption from './table-caption.svelte';
+import Cell from './table-cell.svelte';
+import Footer from './table-footer.svelte';
+import Head from './table-head.svelte';
+import Header from './table-header.svelte';
+import Row from './table-row.svelte';
+import Root from './table.svelte';
+
+const tableCellVariants = tv({
+  base: 'align-middle [&:has([role=checkbox])]:pr-0',
+  variants: {
+    size: {
+      default: 'p-4',
+      sm: 'px-4 py-2',
+      xs: 'px-4 py-0.5',
+      lg: 'p-6',
+    },
+  },
+  defaultVariants: {
+    size: 'default',
+  },
+});
+
+type TableCellProps = {
+  size?: VariantProps<typeof tableCellVariants>['size'];
+};
 
 export {
-	Root,
-	Body,
-	Caption,
-	Cell,
-	Footer,
-	Head,
-	Header,
-	Row,
-	//
-	Root as Table,
-	Body as TableBody,
-	Caption as TableCaption,
-	Cell as TableCell,
-	Footer as TableFooter,
-	Head as TableHead,
-	Header as TableHeader,
-	Row as TableRow,
+  Body,
+  Caption,
+  Cell,
+  Footer,
+  Head,
+  Header,
+  Root,
+  Row,
+  //
+  Root as Table,
+  Body as TableBody,
+  Caption as TableCaption,
+  Cell as TableCell,
+  Footer as TableFooter,
+  Head as TableHead,
+  Header as TableHeader,
+  Row as TableRow,
+  tableCellVariants,
+  type TableCellProps,
 };
--- packages/svelte/src/lib/components/ui/table/table-cell.svelte
+++ packages/svelte/src/lib/components/ui/table/table-cell.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
-	import type { HTMLTdAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils.js";
+  import { cn } from '$lib/utils.js';
+  import type { HTMLTdAttributes } from 'svelte/elements';
+  import { tableCellVariants, type TableCellProps } from './index.js';
 
-	type $$Props = HTMLTdAttributes;
+  type $$Props = HTMLTdAttributes & TableCellProps;
 
-	let className: $$Props["class"] = undefined;
-	export { className as class };
+  let className: $$Props['class'] = undefined;
+  export let size: $$Props['size'] = 'default';
+  export { className as class };
 </script>
 
-<td
-	class={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
-	{...$$restProps}
-	on:click
-	on:keydown
->
-	<slot />
+<td class={cn(tableCellVariants({ size, className }))} {...$$restProps} on:click on:keydown>
+  <slot />
 </td>
