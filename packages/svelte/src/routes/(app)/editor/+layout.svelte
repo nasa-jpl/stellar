1c61adfa2c639e6b856c047b5160b4701711c262
Lyle Klyne <lyle.klyne@gmail.com>
2025-01-30 10:42:11 -0800

    add route to example editor

---
+++ packages/svelte/src/routes/(app)/editor/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  // Empty layout without header/footer
+  import '../../../index.css';
+  // import '../../../theme.css';
+</script>
+
+<div class="p-12">
+  <slot />
+</div>
---
+++ packages/svelte/src/routes/(app)/editor/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Button } from '$lib/index.js';
+</script>
+
+<div class="p-12 bg-red-200">
+  <h1 class="text-2xl font-bold mb-4">Code Editor Example</h1>
+  <Button variant="default" size="xs">Test Button</Button>
+</div>
