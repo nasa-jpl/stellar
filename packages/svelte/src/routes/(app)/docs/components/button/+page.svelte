7a25a9d2aff1898a8faafd79234ae25d858bec58
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-01-30 10:49:25 -0800

    Remove react tab for now in component preview and add styling

--- packages/svelte/src/routes/(app)/docs/components/button/+page.svelte
+++ packages/svelte/src/routes/(app)/docs/components/button/+page.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
   import '@nasa-jpl/stellar';
   // import { Button } from '@nasa-jpl/stellar-react';
+  import Highlight from 'svelte-highlight';
+  import typescript from 'svelte-highlight/languages/typescript';
+  import 'svelte-highlight/styles/monokai.css';
   import ComponentDocs from '../../../../../www/components/component-docs.svelte';
   import Example from './example.svelte';
   import exampleString from './example.svelte?raw';
-  import reactExampleString from './react-example.svelte?raw';
 </script>
 
-<ComponentDocs>
-  <div slot="svelte">
-    <div>Code</div>
-    <Example />
-    <code>{exampleString}</code>
+<ComponentDocs
+  title="Button"
+  description="Displays a button or a component that looks like a button."
+  link="https://www.shadcn-svelte.com/docs/components/button"
+>
+  <div
+    slot="preview"
+    class="ring-offset-background focus-visible:ring-ring mt-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 relative rounded-md border"
+  >
+    <div class="preview flex min-h-[350px] w-full justify-center p-10 items-center">
+      <Example />
+    </div>
   </div>
-  <div slot="react">
-    <div>Code</div>
-    <!-- <ReactAdapter el={Button} children="Button" /> -->
-    <code>{reactExampleString}</code>
+  <div slot="code">
+    <div class="rounded-md overflow-hidden">
+      <Highlight language={typescript} code={exampleString} />
+    </div>
+    <!-- <code>{exampleString}</code> -->
   </div>
 </ComponentDocs>
--- packages/svelte/src/www/components/component-docs.svelte
+++ packages/svelte/src/www/components/component-docs.svelte
@@ -1,29 +1,53 @@
 <script lang="ts">
   import { Tabs } from '$lib/index.js';
+  import { ExternalLink } from 'lucide-svelte';
+
+  export let title: string = 'Title';
+  export let description: string = 'Description of the component';
+  export let link: string = '';
 </script>
 
-<section class="w-full mx-auto flex flex-col items-center gap-2 py-8">
-  <div class="container flex-1 items-start">
-    <Tabs.Root value="svelte" class="relative mr-auto w-full">
+<section class="w-full mx-auto flex flex-col gap-2 py-8">
+  <div class="space-y-2">
+    <h1 class="scroll-m-20 text-4xl font-bold tracking-tight">{title}</h1>
+    <p class="text-muted-foreground text-balance text-lg">{description}</p>
+  </div>
+  {#if link}
+    <div class="flex items-center space-x-2 pt-4">
+      <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+        class="focus:ring-ring inline-flex select-none items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 bg-secondary text-secondary-foreground hover:bg-secondary/80 border-transparent gap-1"
+      >
+        API Reference
+        <ExternalLink size={15} />
+      </a>
+    </div>
+  {/if}
+  <div class="flex-1 items-start pb-12 pt-8">
+    <Tabs.Root value="preview" class="relative mr-auto w-full">
       <div class="flex items-center justify-between pb-3">
         <Tabs.List class="w-full justify-start rounded-none border-b bg-transparent p-0 h-9">
           <Tabs.Trigger
             class="text-muted-foreground data-[state=active]:border-b-primary data-[state=active]:text-foreground relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold shadow-none transition-none data-[state=active]:shadow-none"
-            value="svelte">Svelte</Tabs.Trigger
+            value="preview"
           >
+            Preview
+          </Tabs.Trigger>
           <Tabs.Trigger
             class="text-muted-foreground data-[state=active]:border-b-primary data-[state=active]:text-foreground relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold shadow-none transition-none data-[state=active]:shadow-none"
-            value="react">React</Tabs.Trigger
+            value="code"
           >
+            Code
+          </Tabs.Trigger>
         </Tabs.List>
       </div>
-      <Tabs.Content value="svelte">
-        Svelte
-        <slot name="svelte" />
+      <Tabs.Content value="preview">
+        <slot name="preview" />
       </Tabs.Content>
-      <Tabs.Content value="react"
-        >React
-        <slot name="react" />
+      <Tabs.Content value="code">
+        <slot name="code" />
       </Tabs.Content>
     </Tabs.Root>
   </div>
