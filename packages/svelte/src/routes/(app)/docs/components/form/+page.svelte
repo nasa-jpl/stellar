98a75f8d24b348f8eaca15f8217cdcafc51bb076
Lyle Klyne <lyle.klyne@gmail.com>
2025-02-10 12:04:26 -0800

    fix html typo

--- packages/svelte/src/routes/(app)/docs/components/form/+page.svelte
+++ packages/svelte/src/routes/(app)/docs/components/form/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import P from '$lib/components/ui/typography/P.svelte';
   import ComponentCode from '$src/www/components/component-code.svelte';
   import ComponentExample from '$src/www/components/component-example.svelte';
   import ComponentHeader from '$src/www/components/component-header.svelte';
@@ -15,13 +14,13 @@
     description="Building forms with Formsnap, Superforms, & Zod."
     link="https://www.shadcn-svelte.com/docs/components/form"
   />
-  <P>
+  <p>
     This is a simple example of how to use Formsnap and Superforms together. For a full guide on how to build forms,
     visit the
     <a class="font-medium underline" href="https://www.shadcn-svelte.com/docs/components/form"
       >Shadcn Svelte Form Documentation</a
     >.
-  </P>
+  </p>
   <ComponentExample>
     <Example slot="preview" />
     <ComponentCode slot="code" code={exampleString} />
