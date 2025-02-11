1edb8c81849a4dd86446fea744394e34157486a3
Lyle Klyne <lyle.klyne@gmail.com>
2025-02-11 11:14:43 -0800

    add copy and paste and scroll to code component

--- packages/svelte/src/www/components/component-code.svelte
+++ packages/svelte/src/www/components/component-code.svelte
@@ -1,10 +1,39 @@
 <script lang="ts">
+  import { Check, Copy } from 'lucide-svelte';
   import { Highlight } from 'svelte-highlight';
   import typescript from 'svelte-highlight/languages/typescript';
+  import { cubicInOut } from 'svelte/easing';
+  import { fade } from 'svelte/transition';
 
   export let code: string = '';
+  let copied = false;
+
+  async function copyCode() {
+    await navigator.clipboard.writeText(code);
+    copied = true;
+    setTimeout(() => {
+      copied = false;
+    }, 1000);
+  }
 </script>
 
-<div class="rounded-md overflow-hidden max-h-[500px]">
-  <Highlight language={typescript} {code} />
+<div class="relative rounded-md overflow-hidden">
+  <div class="absolute top-0 right-0 z-20 px-6 py-2">
+    <button on:click={copyCode} class="p-2 bg-gray-800 rounded-md hover:bg-gray-700 transition-colors">
+      <div class="relative w-4 h-4">
+        {#if copied}
+          <div class="absolute inset-0" transition:fade={{ duration: 150, easing: cubicInOut }}>
+            <Check class="w-full h-full text-green-500" />
+          </div>
+        {:else}
+          <div class="absolute inset-0" transition:fade={{ duration: 150, easing: cubicInOut }}>
+            <Copy class="w-full h-full text-gray-300" />
+          </div>
+        {/if}
+      </div>
+    </button>
+  </div>
+  <div class="overflow-auto max-h-[500px]">
+    <Highlight language={typescript} {code} />
+  </div>
 </div>
