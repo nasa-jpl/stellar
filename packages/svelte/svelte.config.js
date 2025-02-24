d4a71333ec487f339bfd2c9ad39c4aacc71ce837
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-02-24 12:24:49 -0800

    Switch to static site for svelte docs

--- packages/svelte/svelte.config.js
+++ packages/svelte/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,16 @@ const config = {
   preprocess: [vitePreprocess()],
 
   kit: {
-    adapter: adapter(),
+    // adapter: adapter(),
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: '200.html', // may differ from host to host
+      precompress: false,
+      strict: true,
+    }),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '',
     },
