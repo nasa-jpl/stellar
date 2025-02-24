d01301245bb9d5c7cd769b1e98ccfb344a6b19ed
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-02-24 15:06:21 -0800

    Link fix

--- packages/svelte/src/routes/(app)/docs/components/+page.ts
+++ packages/svelte/src/routes/(app)/docs/components/+page.ts
@@ -1,6 +1,7 @@
+import { base } from '$app/paths';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types.js';
 
 export const load: PageLoad = async () => {
-  redirect(303, '/docs/components/accordion');
+  redirect(303, `${base}/docs/components/accordion`);
 };
