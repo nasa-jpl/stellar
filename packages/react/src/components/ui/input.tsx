b0eaef749e2a0494f0685fcd89cbe4836566ab8e
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-04-28 16:19:30 -0700

    File input style fixes

--- packages/react/src/components/ui/input.tsx
+++ packages/react/src/components/ui/input.tsx
@@ -22,7 +22,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex w-full rounded-md border border-input bg-background text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+          'flex w-full rounded-md border border-input bg-background text-base ring-offset-background file:h-full file:leading-none file:border-0 file:bg-transparent file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
           inputVariants.size[sizeVariant],
           className,
         )}
--- packages/svelte/src/lib/components/ui/input/input.svelte
+++ packages/svelte/src/lib/components/ui/input/input.svelte
@@ -26,7 +26,7 @@
 
 <input
   class={cn(
-    'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex w-full rounded-md border file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50',
+    'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex w-full rounded-md border file:h-full file:leading-none file:border-0 file:bg-transparent  file:font-medium focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50',
     inputVariants.size[sizeVariant],
     className,
   )}
