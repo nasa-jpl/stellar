beae74c7ba610067ca46daed035b3102c5b7f878
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-06-10 12:02:29 -0700

    Style fixes

--- packages/react/src/components/ui/select.tsx
+++ packages/react/src/components/ui/select.tsx
@@ -49,7 +49,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex w-full items-center justify-between rounded-md border border-input bg-background text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'flex w-full items-center justify-between rounded-md border border-input bg-background text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
       selectVariants.size[size].trigger,
       className,
     )}
