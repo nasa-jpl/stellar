6795d43ae9488ec0b862bb4ff7f73293e46b1de7
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-08-14 07:35:54 -0700

    Fix react select trigger text alignment and word break styling

--- packages/react/src/components/ui/select.tsx
+++ packages/react/src/components/ui/select.tsx
@@ -49,7 +49,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex w-full items-center justify-between rounded-md border border-input bg-background text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'flex w-full items-center justify-between rounded-md border border-input bg-background text-sm ring-offset-background data-[placeholder]:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 text-left break-all',
       selectVariants.size[size].trigger,
       className,
     )}
