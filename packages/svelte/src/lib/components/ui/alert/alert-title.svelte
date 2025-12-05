de7827a7e7937780fab88bc83cfcf63dd9b3f872
AaronPlave <aaronplave@gmail.com>
2025-12-05 14:01:49 -0500

    Alert sizes

--- packages/svelte/src/lib/components/ui/alert/alert-title.svelte
+++ packages/svelte/src/lib/components/ui/alert/alert-title.svelte
@@ -1,21 +1,23 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import type { HeadingLevel } from "./index.js";
-	import { cn } from "$lib/utils.js";
+  import { cn } from '$lib/utils.js';
+  import type { HTMLAttributes } from 'svelte/elements';
+  import { alertSizeVariants, type AlertSize, type HeadingLevel } from './index.js';
 
-	type $$Props = HTMLAttributes<HTMLHeadingElement> & {
-		level?: HeadingLevel;
-	};
+  type $$Props = HTMLAttributes<HTMLHeadingElement> & {
+    level?: HeadingLevel;
+    size?: AlertSize;
+  };
 
-	let className: $$Props["class"] = undefined;
-	export let level: $$Props["level"] = "h5";
-	export { className as class };
+  let className: $$Props['class'] = undefined;
+  export let level: $$Props['level'] = 'h5';
+  export let size: AlertSize = 'default';
+  export { className as class };
 </script>
 
 <svelte:element
-	this={level}
-	class={cn("mb-1 font-medium leading-none tracking-tight", className)}
-	{...$$restProps}
+  this={level}
+  class={cn('mb-1 font-medium leading-none tracking-tight', alertSizeVariants.size[size].title, className)}
+  {...$$restProps}
 >
-	<slot />
+  <slot />
 </svelte:element>
